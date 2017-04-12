972d662aef10f00a49747575c9dcf218f4bc8d99
YOUR NAME <bethstevic89@gmail.com>
2017-04-11 17:35:45 -0700

    Added functions to add messages, and rooms to page

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -5,7 +5,8 @@ var app = {
   rooms: {},
   friends: [],
   currentRoom: '',
-  messages: []
+  messages: [],
+  lastMessageID: 0
 
 };
 
@@ -13,24 +14,39 @@ var app = {
 //display messages retrieved from the parse server
 
 app.init = function() {
-  $('.username').on('click', app.handleUsernameClick());
-  $('.submit').on('click', app.handleSubmit());
+  $('#chats').on('click', '.username', app.handleUsernameClick);
+  $('.submit').on('click', app.handleSubmit);
+  $('#roomSelect').on('change', app.handleRoomChange);
 };
 
-app.fetch = function(message) {
+app.fetch = function() {
   $.ajax({
     url: app.server,
     type: 'GET',
-    data: JSON.stringify(message),
+    data: {},
     contentType: 'application/json',
     success: function(data) {
-      console.log('chatterbox: Message received');
+      //console.log('chatterbox: Message received', data);
+      if (!data.results || !data.results.length) {
+        return;
+      }
+      app.messages = data.results;
+
+      var mostRecentMessage = app.messages[app.messages.length - 1];
+
+      if (mostRecentMessage.objectId !== app.lastMessageID) {
+        app.renderMessage(app.messages);
+        app.renderRoomList(app.messages);
+      }
     },
     error: function(data) {
       console.error('Error!', data);
     }
   });
 };
+
+//setInterval(function() { app.fetch(); }, 10000);
+app.fetch();
 
 app.send = function(message) {
   $.ajax({
@@ -40,7 +56,8 @@ app.send = function(message) {
     data: JSON.stringify(message),
     contentType: 'application/json',
     success: function (data) {
-      console.log('chatterbox: Message sent');
+      console.log('chatterbox: Message sent', data);
+      $('#message').val('');
     },
     error: function (data) {
       // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -50,41 +67,88 @@ app.send = function(message) {
 };
 
 app.clearMessages = function() {
-  $('#chats').children().remove();
+  $('#chats').html('');
 };
 
 app.renderMessage = function(message) {
-  $('#chats').append('<p>' + message + '</p>');
+  app.clearMessages();
+  for (var i = 0; i < message.length; i++) {
+    var currentMessage = message[i].text;
+    var currentUserName = message[i].username;
+    var $chat = $('<div class="chat"><span class= "username"> ' + currentUserName + '</span><br><span>' + currentMessage + '</span></div>');
+
+    $('#chats').prepend($chat);
+  }
+};
+
+
+app.handleUsernameClick = function(e) {
+  //e.preventDefault();
+  var username = e.target.innerText;
+  var slicedUsername = username.slice(0, -2);
+
+  if (username !== undefined) {
+    app.friends[slicedUsername] = !app.friends[slicedUsername];
+  }
+
+  $('.username').each(function() {
+    if (this.innerText === username) {
+      $(this).toggleClass('friend');
+    }
+  });
 
 };
 
-app.renderRoom = function(room) {
-  $('#roomSelect').append('<span>' + room + '</span>');
+app.handleSubmit = function(e) {
+  e.preventDefault();
+  var message = {
+    text: $('#message').val(),
+    username: window.location.search.slice(10),
+    roomname: app.roomname || 'lobby'
+  };
+  app.send(message);
+
+  var $chat = $('<div class="chat"><span class= "username"> ' + message.username + '<span><br><span>' + message.text + '</span></div>');
+  $('#chats').prepend($chat);
 };
 
 
-app.handleUsernameClick = function() {
-  // $('.username').on('click', function() {
-  //   app.friends.push(userName);
-  // });
+app.renderRoomList = function(messages){
+  $('#roomSelect').html('<option vlaue="_newRoom">New Room...</option></select>');
+  if(messages){
+    messages.forEach(function(message){
+      var roomname = message.roomname;
+      if(roomname && !app.rooms[roomname]){
+        app.renderRoom(roomname);
+        app.rooms[roomname] = true;
+      }
+    });
+  }
+  $('#roomSelect').val(app.roomname);
 };
 
-app.handleSubmit = function() {
-
+app.renderRoom = function(roomname) {
+  var option = $('<option/>').val(roomname).text(roomname);
+  $('#roomSelect').append(option);
 };
 
-//protect against XSS attacks
+app.handleRoomChange = function (e) {
+  var selectIndex = $('#roomSelect').prop('selectedIndex');
+    if(selectIndex === 0) {
+      var roomname = prompt('Enter room name');
 
-//Set up a way to refresh displayed messages, either automatically or with a button
+      if(roomname){
+        app.roomname = roomname;
+        app.renderRoom(roomname);
+        $('#roomSelect').val(roomname);
+      }
+    } else {
+      app.roomname = $('#roomSelect').val();
+    }
+    app.renderMessage(app.messages);
+};
 
-//Allow users to select a user name and be able to send messages
 
-
-//Allow users to create rooms and enter existing rooms
-
-//Allow users to 'befriend' other users by clicking on their user name
-
-//Display all messages sent by friends in bold
 
 
 
